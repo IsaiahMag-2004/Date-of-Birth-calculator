2e10baac159fc8954602286dffc027b54fb230fe
Isaiah Magana <Maganaisaiah4@gmail.com>
2022-06-27 21:53:21 -0400

    Created function for Button Date Picker body
    Created a function for the button date clicker so that its code is organized, aswell as we moved the toast from befor into it to test if it works. It dose!

--- app/src/main/java/us/tutorial/dobcalc/MainActivity.kt
+++ app/src/main/java/us/tutorial/dobcalc/MainActivity.kt
@@ -13,8 +13,14 @@ class MainActivity : AppCompatActivity() {
         val buttonDatePicker : Button = findViewById(R.id.date_pick_button) //Gives us access to the button in the UI
 
         buttonDatePicker.setOnClickListener {
-            Toast.makeText(this, "button date picker pressed", Toast.LENGTH_LONG).show()
+            clickDatePicker()
         }
 
+    }
+    /**
+     * Just as discussed we are creating method for the date picker so the code is organized
+     */
+    fun clickDatePicker() {
+        Toast.makeText(this, "button date picker pressed", Toast.LENGTH_LONG).show()    //Just to show if our code is working & should always do so you dont write your whole program than find out it dosnt work
     }
 }
