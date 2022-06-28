d70de7a3d1e12473ff3f5d2e5c58ed9609e2ada8
Isaiah Magana <Maganaisaiah4@gmail.com>
2022-06-27 22:48:42 -0400

    Fully implemented the minutes since date function
    Fully implemented, and tested the function which gets the user how many minutes it has been since a given date

--- app/src/main/java/us/tutorial/dobcalc/MainActivity.kt
+++ app/src/main/java/us/tutorial/dobcalc/MainActivity.kt
@@ -12,7 +12,7 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private var userSelectedDate : TextView? = null //Created here bc if not the date picker function won't see it | make private so that you cant see this while workig with the UI bc it will crash the program
-
+    private var dateInMin: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         buttonDatePicker.setOnClickListener {
             userSelectedDate = findViewById(R.id.selected_date)
+            dateInMin = findViewById(R.id.Minutes_till)
             clickDatePicker()
         }
 
@@ -35,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         val day = myCalander.get(Calendar.DAY_OF_MONTH)
 
         //With the .OnDateSetListener stuff we are basicly using a lamda expression to get 4 parameters all filled with the data the user gives us but we will explain more in future
-        //Anything you put after the -> will be run
+        //Anything y ou put after the -> will be run
         //The Toast will only run when you click ok after choosing a date
         //When using data for months you have to remember that in programming the start of indexes is 0, while the first month of the year - january - is 1
         //Remember you should use (?) to set nullabel when there is a chance there will be no data
@@ -43,10 +44,15 @@ class MainActivity : AppCompatActivity() {
             DatePickerDialog.OnDateSetListener{view, selectedYear, selectedMonth, selectedDayOfMonth ->
                 val selectedDate = "$selectedMonth/$selectedDayOfMonth/$selectedYear"
                 userSelectedDate?.text = selectedDate
-
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-
                 val theDate = sdf.parse(selectedDate)
+
+                val selectedDateInMinutes = theDate.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                val currentDateInMinutes = currentDate.time / 60000
+                val diffrenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                dateInMin?.text = diffrenceInMinutes.toString()
                                               },
             year,
             month,
