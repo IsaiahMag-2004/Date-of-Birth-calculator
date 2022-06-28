0c3b24c7619d3695f60cd0627d5042f7f95492ea
Isaiah Magana <Maganaisaiah4@gmail.com>
2022-06-27 21:49:25 -0400

    Created Button listener
    Created a listener for the date button and a toast to make sure the button was working

--- app/src/main/java/us/tutorial/dobcalc/MainActivity.kt
+++ app/src/main/java/us/tutorial/dobcalc/MainActivity.kt
@@ -2,10 +2,19 @@ package us.tutorial.dobcalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonDatePicker : Button = findViewById(R.id.date_pick_button) //Gives us access to the button in the UI
+
+        buttonDatePicker.setOnClickListener {
+            Toast.makeText(this, "button date picker pressed", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
